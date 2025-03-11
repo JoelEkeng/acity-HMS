dd98a51dfa737531186fded6f571ce1953f23d51
Joel Ekeng <joelekeng23@gmail.com>
2025-03-11 10:28:51 +0000

    Remove ts error

--- components/layout/Button.tsx
+++ components/layout/Button.tsx
@@ -2,8 +2,6 @@ import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
 
-
-//@ts-expect-error (TS 7016) - This import is not used in the code snippet
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
