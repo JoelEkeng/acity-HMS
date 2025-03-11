935d26cf2e5ffb2fd8d8a577aee7b6450f79e3f4
Joel Ekeng <joelekeng23@gmail.com>
2025-03-11 10:10:46 +0000

    Remove auth.ts

--- app/auth/auth.ts
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from "next-auth/next"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
-
-export default async function currentSession() {
-	return await getServerSession(authOptions);
-}
